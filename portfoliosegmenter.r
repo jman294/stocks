9251485d3b55ba771f615d504b63a643989d8a94
Jack DuPlessis <jduplessis294@gmail.com>
2018-10-13 13:01:53 -0400

    Initial commit

---
+++ portfoliosegmenter.r
@@ -0,0 +1,7 @@
+files <- list.files(path="./histdaily")
+stuff <- lapply(files, function(x) {
+  data <- read.csv(file=paste('./histdaily/', x, sep=''), head=FALSE, sep=",") # load file
+  index <- sample(1:nrow(data),round(0.75*nrow(data)))
+  train <- data[index,]
+  test <- data[-index,]
+})
