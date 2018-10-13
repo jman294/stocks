d5c2eeb2f147b40d6b937ef23adcdb4c3b423f7a
Jack DuPlessis <jduplessis294@gmail.com>
2018-10-13 13:27:22 -0400

    Add scaled input values

--- portfoliosegmenter.r
+++ portfoliosegmenter.r
@@ -4,4 +4,12 @@ stuff <- lapply(files, function(x) {
   index <- sample(1:nrow(data),round(0.75*nrow(data)))
   train <- data[index,]
   test <- data[-index,]
+  maxs <- apply(data, 2, max)
+  mins <- apply(data, 2, min)
+
+  scaled <- as.data.frame(scale(data, center = mins, scale = maxs - mins))
+
+  train_ <- scaled[index,]
+  test_ <- scaled[-index,]
+  apply(train_, 2, cat)
 })
