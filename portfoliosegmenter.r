f83f62c2e2e4461015dde2a5689b30012a553cf1
Jack DuPlessis <jduplessis294@gmail.com>
2018-10-13 14:25:51 -0400

    Editing example

--- portfoliosegmenter.r
+++ portfoliosegmenter.r
@@ -1,6 +1,6 @@
 files <- list.files(path="./histdaily", pattern="table_x.*.csv$")
 stuff <- lapply(files, function(x) {
-  data <- read.csv(file=paste('./histdaily/', x, sep=''), head=FALSE, sep=",", colClasses=c("NULL", "NULL", NA, "NULL", "NULL", "NULL", "NULL"))
+  data <- read.csv(file=paste('./histdaily/', x, sep=''), head=TRUE, sep=",", colClasses=c("NULL", "NULL", NA, "NULL", "NULL", "NULL", "NULL"))
   index <- sample(1:nrow(data),round(0.75*nrow(data)))
   train <- data[index,]
   test <- data[-index,]
@@ -11,9 +11,9 @@ stuff <- lapply(files, function(x) {
 
   train_ <- scaled[index,]
   test_ <- scaled[-index,]
-  str(train_)
-  names(train_) <- "price"
-  str(names(train_))
+  #str(train_)
+  #names(train_) <- "price"
+  #str(names(train_))
   library(neuralnet)
   #f <- as.formula(paste(" ~", paste(n[!n %in% "NULL"], collapse = " + ")))
   f <- as.formula("~ price")
