b574b45a59cc856fc829329491dfef2fe94b507c
Jack DuPlessis <jduplessis294@gmail.com>
2018-10-17 21:08:13 -0400

    Start with individual stock analyzer

--- analyzer.r
+++ analyzer.r
@@ -6,10 +6,14 @@ setAs("character", "myDate", function(from) as.Date(from, format="%Y%m%d") )
 
 # Helper functions
 get_stock_data <- function (stock) {
-  raw <- GET(paste('https://api.iextrading.com/1.0/stock/', stock, '/stats', sep=''))
-  content <- NULL
+  rawStats <- GET(paste('https://api.iextrading.com/1.0/stock/', stock, '/stats', sep=''))
+  rawFinancials <- GET(paste('https://api.iextrading.com/1.0/stock/', stock, '/financials', sep=''))
+  contentStats <- NULL
+  contentFinancials <- NULL
   result = tryCatch({
-    content <- fromJSON(rawToChar(raw$content))
+    contentStats <- fromJSON(rawToChar(rawStats$content))
+    contentFinancials <- fromJSON(rawToChar(rawFinancials$content))
+    cat(str(c(contentStats, contentFinancials)))
   }, warning = function(w) {
   }, error = function(e) {
     return(NULL)
@@ -53,22 +57,59 @@ read_stock_data <- function (R_filename) {
 # Percent of combined market cap company holds over past three years
 #                                                   |-Change in market share
 
-get_total_market_share <- function (stock_data) {
+get_total_market_cap <- function (stock_data) {
   total <- 0
-  for (i in data) {
+  for (i in stock_data) {
     total = total + i$marketcap
   }
   return(total)
 }
 
-#stocks <- readRDS('./sectors/energy.rds')
-#cat(str(get_total_market_share('./sectors/energy.csv')))
-
 # Financial Strength
 # Debt to equity ratio maybe? Some other basic financial ratio
+get_average_roa <- function (stock_data) {
+  sum <- 0
+  count <- 0
+  for (i in stock_data) {
+    if (class(i$returnOnAssets) == 'numeric') {
+      sum = sum + i$returnOnAssets
+      count = count + 1
+    }
+  }
+  return(sum/count)
+}
+
 
 # Innovation
 # Percent headlines mentioning company and "new"
 
 # Business model
 # Rated by user for now
+
+#### Prog
+
+if (length(list.files('sectors/rstockdata')) == 0) {
+  cat('Gathering stock data...')
+  write_all_stock_data()
+  cat('done')
+}
+
+for (i in list.files('./sectors/rstockdata')) {
+  stocks <- read_stock_data(paste('./sectors/rstockdata/', i, sep=''))
+  totalMarketCap <- get_total_market_cap(stocks)
+  averageROA <- get_average_roa(stocks)
+  cat('\n\n\n\n')
+  cat(i)
+  cat('\nAverage ROA: ')
+  cat(averageROA)
+  cat('\n')
+  for (stock in stocks) {
+    mktShare <- stock$marketcap/totalMarketCap
+    cat(stock$symbol)
+    cat('\n')
+    cat(mktShare)
+    cat('\nFinancial Strength\n')
+    cat(stock$returnOnAssets - averageROA)
+    cat('\n\n')
+  }
+}
